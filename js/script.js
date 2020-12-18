3fac77ce25a4381188ba9bc045a97f9a0f7021be
Alessio Pancia <alessiop2801@gmail.com>
2020-12-18 15:49:20 +0100

    primi inserimenti

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ var app = new Vue({
     searchMovie: function() {
       var text = this.searchText.split(" ").join("+");
       var questo = this;
+      questo.films = [];
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: "87999777404e3c905e01e7dfe9466bae",
@@ -18,6 +19,17 @@ var app = new Vue({
       }).then(function (response) {
         response.data.results.forEach((element, index) => {
           questo.films.push(response.data.results[index]);
+
+          if(response.data.results[index].backdrop_path == null) {
+            questo.films[index].backdrop_path = "https://via.placeholder.com/220x330.png?text=Cover+Mancante";
+          } else {
+            var link = "https://image.tmdb.org/t/p/w220_and_h330_face/" + response.data.results[index].backdrop_path;
+            questo.films[index].backdrop_path = link;
+          }
+
+          if (response.data.results[index].overview == "") {
+            questo.films[index].overview = "Descrizione mancante";
+          }
         })
       });
 
