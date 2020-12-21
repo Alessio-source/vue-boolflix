33af9d85d38772ec0afb6cc3c0eb5df563749983
Alessio Pancia <alessiop2801@gmail.com>
2020-12-21 20:50:39 +0100

    gestione pagine

--- js/script.js
+++ js/script.js
@@ -7,11 +7,58 @@ var app = new Vue({
     empty: "",
     title1: "",
     title2: "",
+    page1: 1,
+    maxPage1: 1,
+    max1: 11,
+    min1: 2,
+    arrMaxPage1: [],
+    page2: 1,
+    maxPage2: 1,
+    max2: 11,
+    min2: 2,
+    arrMaxPage2: []
   },
   mounted() {
     this.searchMovie();
   },
   methods: {
+    next: function() {
+      if(this.page1 < this.maxPage1) {
+        this.page1++;
+      }
+      this.searchMovie();
+    },
+    back: function() {
+      if(this.page1 > 1) {
+        this.page1--;
+      }
+      this.searchMovie();
+    },
+    page: function(index) {
+      this.page1 = index;
+      this.searchMovie();
+    },
+    nextS: function() {
+      if(this.page2 < this.maxPage2) {
+        this.page2++;
+      }
+      this.searchMovie();
+    },
+    backS: function() {
+      if(this.page2 > 1) {
+        this.page2--;
+      }
+      this.searchMovie();
+    },
+    pageElement: function(index) {
+      this.page2 = index;
+      this.searchMovie();
+    },
+    search: function() {
+      this.page1 = 1;
+      this.page2 = 2;
+      this.searchMovie();
+    },
     searchMovie: function() {
 
       const textSearch = this.searchText.split(" ").join("+");
@@ -31,14 +78,14 @@ var app = new Vue({
           params: {
             api_key: "87999777404e3c905e01e7dfe9466bae",
             language: "it",
-            page: 1,
+            page: elementThis.page1,
           }
         });
         call2 = axios.get('https://api.themoviedb.org/3/movie/top_rated', {
           params: {
             api_key: "87999777404e3c905e01e7dfe9466bae",
             language: "it",
-            page: 1,
+            page: elementThis.page2,
           }
         });
         isSearch = false;
@@ -48,7 +95,7 @@ var app = new Vue({
             api_key: "87999777404e3c905e01e7dfe9466bae",
             language: "it",
             query: textSearch,
-            page: 1,
+            page: elementThis.page1,
           }
         });
         call2 = axios.get('https://api.themoviedb.org/3/search/tv', {
@@ -56,7 +103,7 @@ var app = new Vue({
             api_key: "87999777404e3c905e01e7dfe9466bae",
             language: "it",
             query: textSearch,
-            page: 1,
+            page: elementThis.page2,
           }
         });
         isSearch = true;
@@ -65,6 +112,73 @@ var app = new Vue({
       axios.all([call1, call2]).then(axios.spread((...responseOnes) => {
         const responseOne = responseOnes[0];
         const responseTwo = responseOnes[1];
+
+        function pageElement1() {
+          elementThis.maxPage1 = responseOne.data.total_pages;
+          elementThis.min1 = 2;
+          elementThis.max1 = 11;
+          elementThis.arrMaxPage1 = [];
+
+          while(elementThis.min1 <= elementThis.max1) {
+
+            if (elementThis.max1 <= elementThis.page1) {
+              elementThis.min1 = elementThis.page1 - 1;
+              var number = elementThis.min1 + 10;
+              elementThis.max1 += elementThis.min1;
+            } else {
+              elementThis.arrMaxPage1.push(elementThis.min1);
+            }
+
+            elementThis.min1++;
+          }
+
+          if(elementThis.max1 >= elementThis.maxPage1) {
+
+            elementThis.max1 = elementThis.maxPage1;
+            elementThis.min1 = elementThis.max1 - 9;
+            elementThis.arrMaxPage1 = [];
+
+            while(elementThis.min1 <= elementThis.max1) {
+              elementThis.arrMaxPage1.push(elementThis.min1);
+              elementThis.min1++
+            }
+          }
+        }
+
+        function pageElement2() {
+          elementThis.maxPage2 = responseOne.data.total_pages;
+          elementThis.min2 = 2;
+          elementThis.max2 = 11;
+          elementThis.arrMaxPage2 = [];
+
+          while(elementThis.min2 <= elementThis.max2) {
+
+            if (elementThis.max2 <= elementThis.page2) {
+              elementThis.min2 = elementThis.page2 - 1;
+              var number = elementThis.min2 + 10;
+              elementThis.max2 += elementThis.min2;
+            } else {
+              elementThis.arrMaxPage2.push(elementThis.min2);
+            }
+
+            elementThis.min2++;
+          }
+
+          if(elementThis.max2 >= elementThis.maxPage2) {
+
+            elementThis.max2 = elementThis.maxPage2;
+            elementThis.min2 = elementThis.max2 - 9;
+            elementThis.arrMaxPage2 = [];
+
+            while(elementThis.min2 <= elementThis.max2) {
+              elementThis.arrMaxPage2.push(elementThis.min2);
+              elementThis.min2++
+            }
+          }
+        }
+
+        pageElement1();
+        pageElement2();
 
         one(responseOne);
         second(responseTwo);
@@ -160,6 +274,7 @@ var app = new Vue({
           if(arrSeries == 0) {
             elementThis.title2 = "";
           } else {
+            elementThis.page2 = 1;
             elementThis.title2 = "Serie tv";
           }
         } else {
@@ -173,7 +288,6 @@ var app = new Vue({
           elementThis.empty = "";
         }
 
-        this.searchText = "";
     }))
     },
   }
